328c5936fb50c523f70f445088d3bea71fca7799
Quentin Honnart <quentin.honnart@gmail.com>
2023-07-26 13:33:02 +0200

    project basis

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
+})->middleware('guest')->name('guest.home');
+
+Route::middleware('auth')->group(function () {
+
 });
