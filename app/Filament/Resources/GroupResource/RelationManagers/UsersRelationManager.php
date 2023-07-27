6a31cf08525ed0598e6ec6a700819fecb1cdc47e
Quentin Honnart <quentin.honnart@gmail.com>
2023-07-27 11:56:40 +0000

    dd group resource + attach relation on edit form

---
+++ app/Filament/Resources/GroupResource/RelationManagers/UsersRelationManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources\GroupResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UsersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'users';
+
+    protected static ?string $recordTitleAttribute = 'email';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('email'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\DetachAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DetachBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\AttachAction::make(),
+            ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function groups() {
+        return $this->belongsToMany(Group::class, 'group_user', 'user_id', 'group_id')->withTimestamps()->withPivot('user_id', 'group_id');
+    }
 }
