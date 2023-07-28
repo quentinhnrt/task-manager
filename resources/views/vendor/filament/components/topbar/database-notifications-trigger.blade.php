9590f8731d3f525e288df047948de79517dd77b1
Quentin <qhonnart@amphibee.fr>
2023-07-28 18:48:18 +0200

    remove filament

--- resources/views/vendor/filament/components/topbar/database-notifications-trigger.blade.php
+++
@@ -1,8 +0,0 @@
-<x-filament::icon-button
-    :badge="$unreadNotificationsCount"
-    color="gray"
-    icon="heroicon-o-bell"
-    icon-alias="panels::database-notifications.open-button"
-    icon-size="lg"
-    :label="__('filament::layout.actions.open_database_notifications.label')"
-/>
--- resources/views/vendor/filament/resources/relation-manager.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="fi-resource-relation-manager">
-    {{ \Filament\Support\Facades\FilamentView::renderHook('resource.relation-manager.start', scope: static::class) }}
-
-    {{ $this->table }}
-
-    {{ \Filament\Support\Facades\FilamentView::renderHook('resource.relation-manager.end', scope: static::class) }}
-</div>
